aff51edbc36192995e5436233cbcf074fdd00717
joshua-unseen <corpselight@gmail.com>
2019-07-10 23:59:47 -0700

    Updates

---
+++ precache-manifest.88817d34ce3622b6abb540d4e67435b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a798f546cd5c29a80b3eb788ab03a1",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "2ee4f49bb0b2d29f3a91",
+    "url": "/clicky-game/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/clicky-game/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "2ee4f49bb0b2d29f3a91",
+    "url": "/clicky-game/static/js/main.f2a0298e.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
