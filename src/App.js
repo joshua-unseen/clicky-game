02698b078b7af647e271753e1ec390ef11030dbd
joshua-unseen <corpselight@gmail.com>
2020-02-13 08:42:42 -0800

    Moved styles  to /src/App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import tiles from "./tiles.json"
 import Header from './components/Header';
 import Banner from './components/Banner';
